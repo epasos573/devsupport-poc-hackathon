a1e83dc00b0fa57806499551b0c4d7233d3ae43a
epasos573 <eric.pasos@cloudinary.com>
2025-08-22 00:29:17 +0800

    Added the demo use case workflow

--- controllers/kbase_chklist_controller.py
+++ controllers/kbase_chklist_controller.py
@@ -25,6 +25,139 @@ class KbaseChkListController:
 
 
 
+    def get_ticket_internal_notes(self, ticket_id):
+        # Initialize connection to Zendesk platform
+        zendesk_config = {
+            'zd_url': SSM.get_secret('zendesk_client_url'),
+            'zd_email': SSM.get_secret('zendesk_client_email'),
+            'zd_api_token': SSM.get_secret('zendesk_client_api_token'),
+        }
+
+        # Instantiate the ZendeskClient client
+        zendesk_api_client = ZendeskClient(**zendesk_config)
+
+        # Fetch ticket properties from Zendesk API
+        ticket_all_comments = zendesk_api_client.ticket_list_comments(ticket_id=ticket_id)
+
+        internal_note_data = ticket_all_comments["comments"][3]
+        
+        return internal_note_data
+        
+
+    def get_confluence_data(self, title):
+
+        # ToDo: To be extracted from Confluence Page by title
+        confluence_data = "TODO - feature to be implemented here..."
+
+        return confluence_data
+
+
+    def get_openai_analysis(self, proposed_updates_and_changes, confluence_data, zendesk_kb_data = "",  zendesk_macro_data = ""):
+        """Function to send the context and prompt to OpenAI"""
+
+        # Initialize connection to OpenAI platform
+        openai_client = OpenAiClient()
+
+        # Note: Modify the CONTEXT and PROMPT parameters to be used in the PoC
+        data_context = """
+            You are dev support team and you need to identify if there are some changes needed for the different information sources:
+            **confluence_data**: {confluence_data}
+            **zendesk_kb_data**: {zendesk_kb_data}
+            **zendesk_macro_data**: {zendesk_macro_data}
+        """
+
+        data_prompt  = "You can check the proposed changes: {proposed_updates_and_changes}"
+
+        openai_response_data = openai_client.send_prompt(
+                context=data_context,
+                prompt=data_prompt
+            )        
+        
+        return openai_response_data
+
+
+    def create_confluence_page(self, new_confluence_data):
+
+        # Update the confluence Page here
+
+        # NotYetImplemented
+
+        # Add the code here
+
+        return True
+
+
+
+
+
+    def process_demo_workflow(self):
+        """Function to demo the use cases as detailed for hackathon"""
+        self.logger.info("KbaseChkListController::process_demo_workflow")
+
+        ##############################
+        # Get the Ticket Internal Note
+
+        # Use the ticket to be used of the PoC
+        ticket_id_ref = "333361"
+        internal_notes = self.get_ticket_internal_notes(ticket_id=ticket_id_ref)
+        internal_notes_body = internal_notes['body']
+
+        
+        # We can use hard-coded for the PoC (they can be extracted from internal_notes_body):
+        confluence_kb_title     = "Invalidation after deletion (getting 404 error)"
+        zendesk_kb_title        = "CDN Invalidations: URL conventions invalidated for removed or replaced assets"
+        zendesk_macro_title     = "Delivery::CDN Invalidation::Invalidation settings and switching between URL based to Surrogate-key invalidation (partial - fill in blanks)"
+        
+        proposed_updates_and_changes = "This is just a demo but there might be some information to be updated"
+
+
+
+
+        ##############################
+        # Get the Confluence Page based on Title
+        confluence_data = self.get_confluence_data(confluence_kb_title)
+
+
+
+
+        ##############################
+        # Get Zendesk KB based on title
+        # NotYetImplemented
+
+
+
+        ##############################
+        # Get Zendesk Macro based on title
+        # NotYetImplemented
+
+
+
+        ##############################
+        ### Do the analysis and create new confluence page
+
+        new_confluence_analysis_data = self.get_openai_analysis(proposed_updates_and_changes, confluence_data, zendesk_kb_data = "",  zendesk_macro_data = "")
+
+        self.create_confluence_page(new_confluence_data=new_confluence_analysis_data)
+
+        return {
+            "processing": new_confluence_analysis_data
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def process_openai_request(self):
         """Function to send the context and prompt to OpenAI"""
@@ -65,7 +198,7 @@ class KbaseChkListController:
         # Fetch ticket properties from Zendesk API
         # Use the ticket to be used of the PoC
         ticket_id_ref = "333361"
-        ticket_properties = zendesk_api_client.ticket_show(ticket_id=ticket_id_ref)
+        ticket_properties = zendesk_api_client.ticket_list_comments(ticket_id=ticket_id_ref)
         
         return {
             "Zendesk-Response": ticket_properties
